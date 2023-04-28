985172b8e5412b7ffd7b6e69155fbfe54b7b526d
rharshitaaa21 <raghav.harshita21@gmail.com>
2023-04-28 11:31:10 +0530

    added toggle checkbox

--- src/app/MyComponents/todo-item/todo-item.component.ts
+++ src/app/MyComponents/todo-item/todo-item.component.ts
@@ -2,6 +2,7 @@ import { outputAst } from '@angular/compiler';
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { Title } from '@angular/platform-browser';
 import { Todo } from '../../Todo';
+
 @Component({
   selector: 'app-todo-item',
   templateUrl: './todo-item.component.html',
@@ -11,11 +12,17 @@ export class TodoItemComponent {
   @Input() todo: Todo;
   @Output() todoDelete: EventEmitter<Todo> = new EventEmitter<Todo>();
 
+  @Output() todoCheckbox: EventEmitter<Todo> = new EventEmitter<Todo>();
+
   constructor() {
     this.todo = new Todo(0, '', '', false); // Assign a new instance of Todo to the todo property
   }
 onClick(todo : Todo){
   this.todoDelete.emit(todo);
   console.log("onclick triggered");
+}
+onCheckboxClick(todo: Todo){
+  this.todoCheckbox.emit(todo)
+
 }
 }
--- src/app/MyComponents/todos/todos.component.ts
+++ src/app/MyComponents/todos/todos.component.ts
@@ -29,4 +29,13 @@ constructor() {
     this.todos.push(todo);
     localStorage.setItem("todos", JSON.stringify(this.todos));
   }
+
+
+  toggleTodo(todo:Todo){
+    const index = this.todos.indexOf(todo);
+    this.todos[index].active= !this.todos[index].active ;
+
+
+    localStorage.setItem("todos", JSON.stringify(this.todos));
+  }
 }
